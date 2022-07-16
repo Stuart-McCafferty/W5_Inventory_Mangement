b49a90117f9c3553377522ea96a5d451b939d530
Stuart-McCafferty <Stuartj678@gmail.com>
2022-07-16 23:42:41 +0100

    created supplier model

---
+++ models/supplier.py
@@ -0,0 +1,8 @@
+class Supplier:
+    def __init__(self, _name, _link, _phone,  _id = None,):
+        self.name = _name
+        self.link = _link
+        self.phone = _phone
+        self.id = _id
+    
+    
