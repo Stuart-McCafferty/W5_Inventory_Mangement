6fc0dc109d5c3cc7cffb8cb60e55c97e2ffb9a7b
Stuart-McCafferty <Stuartj678@gmail.com>
2022-07-16 23:18:29 +0100

    created tables and database

---
+++ db/inventory_manager.sql
@@ -0,0 +1,21 @@
+-- Deleting tables before creating again
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS suppliers;
+
+
+CREATE TABLE suppliers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    link VARCHAR(255),
+    phone INT
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    quantity DECIMAL(9,2),
+    cost DECIMAL(9,2),
+    selling_price DECIMAL(9,2),
+    supplier_id INT NOT NULL REFERENCES suppliers(id)
+);
+
